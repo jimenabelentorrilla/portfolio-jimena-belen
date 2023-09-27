a1a7d395e4eeb40bd4fadd3c1002b767b5b6f267
Jimena Belen <jiment4@hotmail.com>
2023-09-27 16:00:34 -0300

    About

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+const About = () => {
+  return (
+    <>
+        <Container>
+          <Row className="row-about">
+              <Col>
+                  <div>
+                    
+                  </div>
+              </Col>
+              
+          </Row>
+        </Container>
+    </>  
+  )
+}
+
+export default About;
