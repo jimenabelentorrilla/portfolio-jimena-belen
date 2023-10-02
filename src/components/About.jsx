78bbd252aa1a57beaf7bb2b68771a2992728b802
Jimena Belen <jiment4@hotmail.com>
2023-10-02 15:23:04 -0300

    paragraph about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,8 @@ const About = () => {
                     </div>
 
                     <div>
-                        <p className="text-bio">En el cruce de la lógica y la creatividad, me desempeño como desarrolladora con experiencia en <strong>React, JavaScript, Bootstrap y CSS </strong>buscando constantemente desafíos intrigantes y la elaboración de un diseño elegante. Me motiva la capacidad de la tecnología para establecer conexiones significativas y enriquecer experiencias digitales.</p>
+                        <p className="text-bio">En el cruce de la lógica y la creatividad, me desempeño como desarrolladora con experiencia en <strong>React, JavaScript, Bootstrap y CSS </strong>buscando constantemente desafíos intrigantes.</p>
+                        <p className="text-bio"> Me motiva la capacidad de la tecnología para establecer conexiones significativas y enriquecer experiencias digitales.</p>
                     </div>
                     </div>
                 </Col>               
