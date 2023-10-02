671d01f9f2d2e287065d3e94a8b066a8c875efcb
Jimena Belen <jiment4@hotmail.com>
2023-10-02 14:00:22 -0300

    Email.js y modal msj enviado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,37 @@
+import React, { useRef, useState  } from 'react';
+import emailjs from '@emailjs/browser';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import Verificar from "../assets/verificar.svg";
+
+import Modal from 'react-bootstrap/Modal';
+
+
 
 const Footer = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wvoajub', 'template_oiaxyzh', form.current, 'vyErkCvr_lW2IRIw7')
+      .then((result) => {
+          console.log(result.text);
+          form.current.reset();
+          setShow(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
         <Container  id="contacto" className="row-footer" >
@@ -23,29 +50,47 @@ const Footer = () => {
                     </div>
                 </Col>
                 <Col>
-                    <form>
+                    <form ref={form} onSubmit={sendEmail}>
                         <div className="flex-input">
                             <div className="input-footer">
-                                <input  type='text' placeholder='Nombre'></input>
+                                <input name="user_name" type='text' placeholder='Nombre' required></input>
                             </div>
                             <div className="input-footer">
-                                <input type="email" placeholder='Email'></input>
+                                <input name="user_email" type="email" placeholder='Email' required ></input>
                             </div>
 
                             <div>
                                 <textarea 
-                                    name="textarea" 
+                                    name="message" 
                                     className="text-area"
-                                    placeholder='Escribí tu mensaje acá' />
+                                    placeholder='Escribí tu mensaje acá'
+                                    required />
                             </div>
                         </div>
                         <div>
-                            <button type="submit" className="btn-form">Enviar</button>
+                            <button type="submit" value="Send" className="btn-form">Enviar</button>
                         </div>
                     </form>
                 </Col>              
           </Row>
         </Container>
+
+        <Modal centered size="md" show={show} onHide={handleClose}>
+            <Modal.Header closeButton> </Modal.Header>
+        
+            <Modal.Body>
+                <div>
+                    <div className='Modal-flex-img'>
+                        <img src={Verificar} />
+                    </div> 
+                    <div className='Modal-text'>
+                        <h5>¡Gracias por contactarme!</h5>
+                        <p>Tu mensaje se ha enviado correctamente.</p>
+                    </div>
+                </div> 
+            </Modal.Body>
+            <Modal.Footer></Modal.Footer>
+        </Modal>
     </>  
   )
 }
