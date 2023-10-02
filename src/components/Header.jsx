be5f33f489b5bb4d8189599b4f475785bd67a033
Jimena Belen <jiment4@hotmail.com>
2023-10-02 16:30:42 -0300

    animate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,31 @@
+import 'animate.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Undraw from "../assets/undraw.svg";
+import TrackVisibility from 'react-on-screen';
 
 const Header = () => {
   return (
     <>
         <Container>
-          <Row className="row-header">
-              <Col>
-                  <div className="text-hd">
-                    <span>Frontend Developer</span>
-                    <h1>Jimena Belén Torrilla</h1>
-                    <a class="btn-des">DESCARGAR CV</a>
+          <TrackVisibility>
+            {({isVisible}) =>
+            <Row className={`row-header ${isVisible ? "animate__animated animate__fadeIn" : ""}`}>
+                <Col>
+                    <div className="text-hd">
+                        <span>Frontend Developer</span>
+                        <h1>Jimena Belén Torrilla</h1>
+                        <a class="btn-des">DESCARGAR CV</a>
+                    </div>
+                </Col>
+                <Col>
+                  <div>
+                    <img className="img-hd" src={Undraw}/>
                   </div>
-              </Col>
-              <Col>
-                <img className="img-hd" src={Undraw}/> 
-              </Col>
-          </Row>
+                </Col>
+            </Row> }
+          </TrackVisibility>
         </Container>
     </>  
   )
--- src/components/Skillss.jsx
+++ src/components/Skillss.jsx
@@ -1,3 +1,4 @@
+import 'animate.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -11,12 +12,12 @@ const Skillss = () => {
           <Row className="row-skills">
               <Col>
                   <div className='flex-skills'>
-                    <FontAwesomeIcon icon={faReact} size="4x" style={{ color: '#2F2E41'}} />
-                    <FontAwesomeIcon icon={faCss3} size="4x" style={{ color: '#2F2E41'}}/>
-                    <FontAwesomeIcon icon={faJs} size="4x" style={{ color: '#2F2E41'}}/> 
-                    <FontAwesomeIcon icon={faBootstrap} size="4x" style={{ color: '#2F2E41'}}/> 
-                    <FontAwesomeIcon icon={faSass} size="4x" style={{ color: '#2F2E41'}}/> 
-                    <FontAwesomeIcon icon={faGit} size="4x" style={{ color: '#2F2E41'}}/> 
+                    <FontAwesomeIcon className="animate__animated animate__fadeInLeft animate__delay-0.5" icon={faReact} size="4x" style={{ color: '#2F2E41'}} />
+                    <FontAwesomeIcon className="animate__animated animate__fadeInLeft animate__delay-1s" icon={faCss3} size="4x" style={{ color: '#2F2E41'}}/>
+                    <FontAwesomeIcon className="animate__animated animate__fadeInLeft animate__delay-2s	" icon={faJs} size="4x" style={{ color: '#2F2E41'}}/> 
+                    <FontAwesomeIcon className="animate__animated animate__fadeInLeft animate__delay-3s	" icon={faBootstrap} size="4x" style={{ color: '#2F2E41'}}/> 
+                    <FontAwesomeIcon className="animate__animated animate__fadeInLeft animate__delay-4s	" icon={faSass} size="4x" style={{ color: '#2F2E41'}}/> 
+                    <FontAwesomeIcon className="animate__animated animate__fadeInLeft animate__delay-5s	" icon={faGit} size="4x" style={{ color: '#2F2E41'}}/> 
                   </div>
               </Col>
           </Row>
