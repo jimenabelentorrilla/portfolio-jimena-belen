04dec3435912cec1860bbc94d0061a4383dcdadc
Jimena Belen <jiment4@hotmail.com>
2023-10-04 15:52:21 -0300

    offcanvas nav

--- src/components/Nav/Naav.jsx
+++ src/components/Nav/Naav.jsx
@@ -1,10 +1,19 @@
 import './Nav.css';
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Menu from "../../assets/Menu.png";
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 
 const Naav = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
         <Container>
@@ -16,7 +25,7 @@ const Naav = () => {
                 </Col>
                 <Col xs={6} sm={10} md={8} className="flex-nav">
                     <div>
-                        <img className="menu" src={Menu}/>
+                        <img onClick={handleShow} className="menu" src={Menu}/>
                     </div>
                     <div>
                         <ul className='nav-ul'>
@@ -27,7 +36,20 @@ const Naav = () => {
                     </div>
                 </Col>
             </Row>
-        </Container>
+    </Container>
+
+    <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton><Offcanvas.Title></Offcanvas.Title></Offcanvas.Header>
+        <Offcanvas.Body>
+            <div>
+                <ul className='nav-hm'>
+                    <a href="#proyectos"><li>Proyectos</li></a>
+                    <a href="#about"><li>Sobre mi</li></a>
+                    <a href="#contacto"><li>Contacto</li></a>
+                </ul>
+            </div>
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   )
 }
